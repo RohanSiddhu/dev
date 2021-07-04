fc421854ef547fc5aeb53eb7bcd1ae21838efeb2
rohansiddhu <rohansiddhu21@gmail.com>
2021-07-04 10:33:54 +0530

    Determinant added in cpp

---
+++ cpp/Determinant/Determinant.h
@@ -0,0 +1,13 @@
+﻿// Determinant.h : Include file for standard system include files.
+
+#pragma once
+
+#include <iostream>
+
+
+/**
+ * @brief Determinant: return the determinant of a 2 x 2 matrix.
+ */
+
+double Determinant(double* matrix, int size);
+void PrintDet(double* matrix, int size);
