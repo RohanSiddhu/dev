ef06f7b7640dd053e9056d5d8837172fc15b8407
rohansiddhu <rohansiddhu21@gmail.com>
2021-09-14 11:03:44 +0530

    Added InsertionSort

---
+++ InsertionSort/InsertionSort/InsertionSort.cxx
@@ -0,0 +1,36 @@
+﻿/**
+ * InsertionSort.cpp : Defines the entry point for the application.
+ */
+
+#include "InsertionSort.h"
+
+
+long ARR_SIZE = 200000l;
+
+
+int main() {
+	std::cout << "Enter size of array: ";
+	std::cin >> ARR_SIZE;
+	int32_t* arr = new int32_t[ARR_SIZE];
+
+	for (long i = 0; i < ARR_SIZE; i++) {
+		arr[i] = ARR_SIZE - i;
+	}
+
+	{
+		Time t;
+		for (long i = 1; i < ARR_SIZE; i++) {
+			long key = arr[i];
+			long j = i - 1;
+
+			while (j >= 0 && arr[j] > key) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+			arr[j + 1] = key;
+		}
+	}
+
+	delete[] arr;
+	return 0;
+}
---
+++ InsertionSort/InsertionSort/InsertionSort.h
@@ -0,0 +1,63 @@
+﻿/**
+ * InsertionSort.h : Header file for InsertionSort.cxx
+ */
+
+#pragma once
+
+
+// Include Headers.
+
+#include <iostream>
+
+#ifdef _WIN64
+#include <Windows.h>
+#endif
+
+#ifdef __linux__
+#include <ctime>
+#endif
+
+
+#ifdef _WIN64
+class Time {
+private:
+	LARGE_INTEGER start, end, frequency, duration;
+public:
+	Time() {
+		QueryPerformanceFrequency(&frequency);
+		QueryPerformanceCounter(&start);
+	}
+
+	~Time() {
+		QueryPerformanceCounter(&end);
+		duration.QuadPart = end.QuadPart - start.QuadPart;
+		duration.QuadPart *= 1000000;
+		duration.QuadPart /= frequency.QuadPart;
+
+		std::cout << "Time: " << (double)duration.QuadPart / 1000000 << "s\n";
+	}
+};
+#elif defined __linux__
+class Time {
+private:
+	timespec start, end, duration;
+public:
+	Time() {
+		clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
+	}
+
+	~Time() {
+		clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+		if ((end.tv_nsec - start.tv_nsec) < 0) {
+			duration.tv_sec = end.tv_sec - start.tv_sec - 1;
+			duration.tv_nsec = 1000000000 + end.tv_nsec - start.tv_nsec;
+		}
+		else {
+			duration.tv_sec = end.tv_sec - start.tv_sec;
+			duration.tv_nsec = end.tv_nsec - start.tv_nsec;
+		}
+
+		std::cout << "Time: " << duration.tv_sec << "." << duration.tv_nsec << "s\n";
+	}
+};
+#endif
