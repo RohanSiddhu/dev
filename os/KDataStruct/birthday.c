c13e44ceaa87ce7a486eaf89cfbc7effda017a84
rohansiddhu <rohansiddhu21@gmail.com>
2022-05-21 20:21:00 +0530

    KDataStruct completed and tested

--- os/KDataStruct/birthday.c
+++ os/KDataStruct/birthday.c
@@ -6,6 +6,7 @@
 #include <linux/slab.h>
 
 struct birthday {
+    char name[33];
     int day;
     int month;
     int year;
@@ -15,10 +16,15 @@ struct birthday {
 LIST_HEAD(birthday_list);
 
 /* This function append birthday node to the given list. */
-void append_birthday(struct list_head *list, int day, int month, int year) {
+void append_birthday(struct list_head *list, const char* name, int day, int month, int year) {
     struct birthday *person;
+    int i;
 
     person = kmalloc(sizeof(*person), GFP_KERNEL);
+    for (i = 0; name[i] != 0; i++) {
+        person->name[i] = name[i];
+    }
+    person->name[i] = 0;
     person->day = day;
     person->month = month;
     person->year = year;
@@ -32,14 +38,18 @@ int birthday_init(void) {
     printk(KERN_INFO "Loading Module\n");
 
     // Inserting elements to birthday_list.
-    append_birthday(&birthday_list, 2, 8, 1995);
+    append_birthday(&birthday_list, "Shivani Siddhu", 21, 11, 1995);
+    append_birthday(&birthday_list, "Rohan Siddhu", 17, 10, 2002);
+    append_birthday(&birthday_list, "Eshant Siddhu", 25, 12, 2000);
+    append_birthday(&birthday_list, "Anshuman Siddhu", 29, 8, 2010);
+    append_birthday(&birthday_list, "Tarun Siddhu", 16, 8, 1974);
 
     // Traversing birthday_list.
     struct birthday *ptr;
 
     list_for_each_entry(ptr, &birthday_list, list) {
-        printk(KERN_INFO "Birthday (dd/mm/yyyy): %2d/%2d/%4d\n",
-                ptr->day, ptr->month, ptr->year);
+        printk(KERN_INFO "%s\'s birthday (dd/mm/yyyy): %02d/%02d/%04d\n",
+                ptr->name, ptr->day, ptr->month, ptr->year);
     }
 
     return 0;
