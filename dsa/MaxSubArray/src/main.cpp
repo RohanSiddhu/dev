0be64a93a0604f7061422b0d982314506902057d
Rohan Siddhu <rohansiddhu21@gmail.com>
2022-10-06 23:40:14 +0530

    Added Time class

--- dsa/MaxSubArray/src/main.cpp
+++ dsa/MaxSubArray/src/main.cpp
@@ -1,18 +1,37 @@
 #include "MaxSubArray.hpp"
 #include <iostream>
+#include <chrono>
+
+
+class Time {
+public:
+    std::chrono::steady_clock::time_point start;
+    std::chrono::steady_clock::time_point end;
+    std::chrono::duration<double> duration;
+public:
+    Time() {
+        start = std::chrono::high_resolution_clock::now();
+    }
+
+    ~Time() {
+        end = std::chrono::high_resolution_clock::now();
+        duration = end - start;
+
+        std::cout.setf(std::ios::floatfield, std::ios::fixed);
+        std::cout << duration.count() << std::endl;
+    }
+};
 
 
 int main() {
     int arr[] {13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7};
     MaxArray maxArr {0};
 
-    //maxArr = max_subarray_divide_and_conquer(arr, 0, sizeof(arr) / sizeof(int) - 1);
-    maxArr = max_subarray_brute_force(arr, 0, sizeof(arr) / sizeof(int) - 1);
-
-    for (int i = maxArr.low; i <= maxArr.high; i++) {
-        std::cout << arr[i] << " ";
+    {
+        Time time;
+        maxArr = max_subarray_divide_and_conquer(arr, 0, sizeof(arr) / sizeof(int) - 1);
+        //maxArr = max_subarray_brute_force(arr, 0, sizeof(arr) / sizeof(int) - 1);
     }
-    std::cout << std::endl;
 
     return EXIT_SUCCESS;
 }
