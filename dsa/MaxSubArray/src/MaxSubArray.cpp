a16eeddeaea4b2eeabaacc77dc4e457364add46b
Rohan Siddhu <rohansiddhu21@gmail.com>
2022-10-06 22:57:43 +0530

    Added Brute Force method

--- dsa/MaxSubArray/src/MaxSubArray.cpp
+++ dsa/MaxSubArray/src/MaxSubArray.cpp
@@ -1,6 +1,16 @@
 #include "MaxSubArray.hpp"
 
 
+/**
+ * @brief Find the maximum sub-array which is crossing the left and right arrays.
+ * 
+ * @param arr Input array.
+ * @param low Lower bound.
+ * @param mid Middle index to the array.
+ * @param high Upper bound.
+ * 
+ * @return MaxArray
+ */
 MaxArray max_crossing_subarray(int arr[], int low, int mid, int high) {
     long long leftSum = arr[mid], sum = 0;
     int leftBound = mid;
@@ -28,14 +38,15 @@ MaxArray max_crossing_subarray(int arr[], int low, int mid, int high) {
     return MaxArray {leftBound, rightBound, leftSum + rightSum};
 }
 
-MaxArray max_subarray(int arr[], int low, int high) {
+
+MaxArray max_subarray_divide_and_conquer(int arr[], int low, int high) {
     if (low == high) {
         return MaxArray {low, high, arr[low]};
     }
     else {
         int mid = (low + high) / 2;
-        MaxArray leftMax = max_subarray(arr, low, mid);
-        MaxArray rightMax = max_subarray(arr, mid + 1, high);
+        MaxArray leftMax = max_subarray_divide_and_conquer(arr, low, mid);
+        MaxArray rightMax = max_subarray_divide_and_conquer(arr, mid + 1, high);
         MaxArray crossMax = max_crossing_subarray(arr, low, mid, high);
 
         if (leftMax.sum > rightMax.sum && leftMax.sum > crossMax.sum) {
@@ -50,4 +61,24 @@ MaxArray max_subarray(int arr[], int low, int high) {
     }
 
     return MaxArray {0};
+}
+
+
+MaxArray max_subarray_brute_force(int arr[], int low, int high) {
+    long long maxSum = arr[0], sum;
+    int lowerBound = 0, upperBound = 0;
+
+    for (int i = 0; i <= high; i++) {
+        sum = arr[i];
+        for (int j = i + 1; j <= high; j++) {
+            sum += arr[j];
+            if (sum > maxSum) {
+                maxSum = sum;
+                lowerBound = i;
+                upperBound = j;
+            }
+        }
+    }
+
+    return MaxArray{ lowerBound, upperBound, maxSum };
 }
--- dsa/MaxSubArray/src/MaxSubArray.hpp
+++ dsa/MaxSubArray/src/MaxSubArray.hpp
@@ -8,4 +8,5 @@ struct MaxArray {
 };
 
 MaxArray max_crossing_subarray(int arr[], int low, int mid, int high);
-MaxArray max_subarray(int arr[], int low, int high);
+MaxArray max_subarray_divide_and_conquer(int arr[], int low, int high);
+MaxArray max_subarray_brute_force(int arr[], int low, int high);
