daa14c96d1746a7b62f5c734f215bc9299db48e0
Rohan Siddhu <rohansiddhu21@gmail.com>
2022-10-09 19:29:17 +0530

    Improved Plot

--- dsa/MaxSubArray/Plot/plot.py
+++ dsa/MaxSubArray/Plot/plot.py
@@ -2,12 +2,28 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("data3.csv")
+df = pd.read_csv("data1.csv")
 
 input = df['input_size'].to_numpy()
 divide_and_conquer = df['divide_and_conquer'].to_numpy()
 brute_force = df['brute_force'].to_numpy()
 
-plt.plot(input, divide_and_conquer)
-plt.plot(input, brute_force)
+x = np.linspace(1, 1000, 1000)
+y2 = x ** 2
+y = x * np.log2(x)
+
+fig1, (ax1, ax2) = plt.subplots(1, 2)
+ax1.plot(x, y2)
+ax2.plot(input, brute_force)
+
+fig2, (ax1, ax2) = plt.subplots(1, 2)
+ax1.plot(x, y)
+ax2.plot(input, divide_and_conquer)
+
+fig3, (ax1, ax2) = plt.subplots(1, 2)
+ax1.plot(x, y)
+ax1.plot(x, y2)
+ax2.plot(input, divide_and_conquer)
+ax2.plot(input, brute_force)
+
 plt.show()
