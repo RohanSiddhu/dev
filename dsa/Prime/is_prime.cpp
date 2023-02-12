9102a0abcec773c455b3f3bf88eb82df1f0b6f73
Rohan Siddhu <48413269+rohansiddhu@users.noreply.github.com>
2023-02-12 13:06:00 +0530

    Update is_prime.cpp

--- dsa/Prime/is_prime.cpp
+++ dsa/Prime/is_prime.cpp
@@ -4,24 +4,16 @@ bool is_prime(uint64_t num) {
     uint64_t limit = num;
 
     if (num <= 1)
-    {
         return false;
-    }
-    else
-    {
+    else {
         uint64_t mod;
-        for (uint64_t i = 2; i <= num; i++)
-        {
+        for (uint64_t i = 2; i <= num; i++) {
             mod = num % i;
 
             if (i >= limit)
-            {
                 return true;
-            }
             else if (mod == 0)
-            {
                 return false;
-            }
             
             limit = num / i;
         }
