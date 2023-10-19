3a62655a71eb25d19cf56f053bc35bdb6e0c14e3
Rohan Siddhu <rohansiddhu21@gmail.com>
2023-10-19 18:50:16 +0530

    Created mcts

---
+++ mcts/mcts/mcts.cpp
@@ -0,0 +1,12 @@
+﻿// mcts.cpp : Defines the entry point for the application.
+//
+
+#include "mcts.h"
+
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+
+	return EXIT_SUCCESS;
+}
---
+++ mcts/mcts/mcts.h
@@ -0,0 +1,7 @@
+﻿// mcts.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <cstdlib>
